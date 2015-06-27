1b6f7343a768ed140017bc14b802b885c7a5ba41
Geoffrey Roberts <g.roberts@blackicemedia.com>
2015-06-27 22:54:29 +1000

    Fixing styling bugs

--- vadialog-unobtrusive.js
+++ vadialog-unobtrusive.js
@@ -134,7 +134,7 @@ $(document).ready(function(){
             width: "100%",
             height: "100%"
           });
-          this.append(f);
+          this.find('[data-vadialog-iframe-trigger]').append(f);
           visionaustralia.resizeDialog(
             this,
             $(window).innerWidth(),
